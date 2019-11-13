b9c24160ba2eaf55baf2e38e8edfabbd1c2beab2
llllJokerllll <Yosi15@gmail.com>
2019-11-13 20:57:25 +0100

    update

---
+++ Trimestre1/Java/Ejercicios_Arrays/src/Ejercicio_Array_2_JoseManuelSabaris.java
@@ -0,0 +1,41 @@
+/**
+ * @author Jose Manuel Sabarís
+ * Título: Ejercicio 
+ * @version 1.0 
+ */
+// Hacer un programa que le de valores a una matriz unitaria de orden 10. 
+// Una matriz unitaria es aquella en la que todos sus elementos valen 0, exceptuando los de la diagonal principal que valen 1.
+
+// Importación de librerías
+
+public class Ejercicio_Array_2_JoseManuelSabaris {
+
+    public static void main( String[] args ) {
+        
+        // Declaración de variables
+        // Entrada de datos
+        // Salida de datos
+        int[][] matrizUnitaria = new int[ 10 ][ 10 ];
+        // Operativa
+        int contador1, contador2;
+        // Constantes
+        // Inicialización
+        
+        // Creación de objetos
+        
+        // Entrada de datos, resolución del programa y salida
+        for ( contador1 = 0; contador1 < matrizUnitaria.length; contador1++ ) {
+            for ( contador2 = 0; contador2 < matrizUnitaria[ contador1 ].length; contador2++ ) {
+                if ( contador1 == contador2 ) {
+                    matrizUnitaria[ contador1 ][ contador2 ] = 1;
+                }
+                System.out.printf( "%d  ", matrizUnitaria[ contador1 ][ contador2 ] );
+            }
+            System.out.println( "" );
+        }
+       
+
+    } // fin de main
+    
+} // fin de la clase Ejercicio_Array_2_JoseManuelSabaris
+
--- Trimestre1/Java/Ejercicios_Arrays/src/Ejercicio_Array_3_JoseManuelSabaris.java
+++ Trimestre1/Java/Ejercicios_Arrays/src/Ejercicio_Array_3_JoseManuelSabaris.java
@@ -3,18 +3,22 @@
  * Título: Ejercicio 
  * @version 1.0 
  */
-//
+// Programa que le de valores al azar comprendidos entre 0 y 31 a una tabla de dimensiones 10x10.
+// Escribir la tabla para comprobar que tiene los valores correctos.
 
 // Importación de librerías
+import java.util.Random;
 
-public class Ejercicio_Array_1_JoseManuelSabaris {
+public class Ejercicio_Array_3_JoseManuelSabaris {
 
     public static void main( String[] args ) {
         
         // Declaración de variables
         // Entrada de datos
         // Salida de datos
+        int[][] matriz = new int[ 10 ][ 10 ];
         // Operativa
+        Random valorAzar = new Random(32);
         // Constantes
         // Inicialización
         
@@ -23,11 +27,10 @@ public class Ejercicio_Array_1_JoseManuelSabaris {
         // Entrada de datos y resolución del programa
 
         
-        
         // Salida de datos
 
 
     } // fin de main
     
-} // fin de la clase Ejercicio_Array_1_JoseManuelSabaris
+} // fin de la clase Ejercicio_Array_3_JoseManuelSabaris
 
