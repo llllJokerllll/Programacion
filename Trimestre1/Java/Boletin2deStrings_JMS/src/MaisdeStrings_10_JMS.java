c3a5f7b91ae08b9baf28f7f719f7a42c64d7d7be
llllJokerllll <Yosi15@gmail.com>
2019-11-20 11:49:39 +0100

    update

---
+++ Trimestre1/Java/Boletin2deStrings_JMS/src/MaisdeStrings_10_JMS.java
@@ -0,0 +1,77 @@
+/*
+ * Documento creado no 2019 por DAWDU10
+ * CIFP A Carballeira. Proyecto Boletin2deStrings_JMS en 20 nov. 2019
+ * Contenido con licencia Creative Commons CC BY-NC-SA.
+ */
+
+/**
+ * @author Jose Manuel Sabarís García <Josemsabaris@gmail.com>
+ * Título: Ejercicio MaisdeStrings_10
+ * @version 1.0
+ */
+// Crear una función contrasinalForte que reciba la contraseña. Deberá verificar que:
+// a. La longitud sea como mínimo de 8.
+// b. Deberá tener 2 o más mayúsculas, 1 o más minúscula y más de 5 números.
+// c. La función indicará si es o no Fuerte la contraseña evaluada.
+
+// Importación de librerías
+import java.util.Scanner;
+
+public class MaisdeStrings_10_JMS {
+
+    public static void main( String[] args ) {
+        
+        // Declaración de variables
+        // Entrada de datos
+        String contrasinal;
+        // Salida de datos
+        // Operativa
+        // Constantes
+        // Inicialización        
+        // Creación de objetos
+        Scanner entrada = new Scanner( System.in );
+        
+        // Entrada de datos
+        System.out.println( "Por favor, introduzca su contraseña" );
+        contrasinal = entrada.next();
+        
+        // Resolucion del programa y salida de datos
+        contrasinalForte( contrasinal );
+
+    } // fin de main
+    public static void contrasinalForte ( String cadena ) {
+        
+        int auxMayus, auxMinus, auxNum;
+        int contador1, contador2, contadorNums;
+        final String MAYUS = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+        final String MINUS = "abcdefghijklmnñopqrstuvwxyz";
+        final String NUMS = "0123456789";
+        auxMayus = auxMinus = auxNum = 0;
+        
+        if ( cadena.length() < 8 ) {
+            System.out.println( "Contraseña demasiado corta" );
+        } else {
+            for ( contador1 = 0; contador1 < cadena.length(); contador1++ ){
+                contadorNums = 0;
+                for ( contador2 = 0; contador2 < MAYUS.length(); contador2++ ) {
+                    if ( cadena.charAt( contador1 ) == MAYUS.charAt( contador2 ) ) {
+                        auxMayus++;
+                    } else if ( cadena.charAt( contador1 ) == MINUS.charAt( contador2 ) ) {
+                        auxMinus++;
+                    } else if ( contadorNums < 9 ) {
+                        if ( cadena.charAt( contador1 ) == NUMS.charAt( contadorNums ) ) {
+                            auxNum++;                           
+                        }
+                        contadorNums++;
+                    }
+                }
+            }
+            if ( auxMayus >= 2 && auxMinus >= 1 && auxNum > 5 ) {
+                System.out.println( "Contraseña fuerte" );
+            } else {
+                System.out.println( "Contraseña débil" );
+            }
+        }
+    }
+} // fin de la clase MaisdeStrings_10_JMS
+
--- Trimestre1/Java/Boletin2deStrings_JMS/src/MaisdeStrings_9_JMS.java
+++ Trimestre1/Java/Boletin2deStrings_JMS/src/MaisdeStrings_9_JMS.java
@@ -19,31 +19,46 @@ import java.util.Scanner;
 public class MaisdeStrings_9_JMS {
 
     public static void main( String[] args ) {
-        
+
         // Declaración de variables
         // Entrada de datos
+        double peso, altura;
         // Salida de datos
+        double resultado;
         // Operativa
         // Constantes
         // Inicialización        
         // Creación de objetos
-        
-        // Entrada de datos y resolución del programa
+        Scanner entrada = new Scanner( System.in );
 
-        
-        // Salida de datos
+        // Entrada de datos
+        System.out.println( "Por favor, introduzca el peso en KG" );
+        peso = entrada.nextDouble();
+        System.out.println( "Por favor, introduzca la altura en metros" );
+        altura = entrada.nextDouble();
 
+        // Resolución del programa y salida
+        resultado = iMasaCorporal( peso, altura );
+        if ( resultado < 18.5 ) {
+            System.out.println( "Por debajo del peso" );
+        } else if ( resultado >= 18.5 && resultado <= 24.9 ) {
+            System.out.println( "Saludable" );
+        } else if ( resultado >= 25.0 && resultado <= 29.9 ) {
+            System.out.println( "Con sobrepeso" );
+        } else if ( resultado >= 30.0 && resultado <= 39.9 ) {
+            System.out.println( "Obeso" );
+        } else {
+            System.out.println( "Obesidad extrema o alto riesgo" );
+        }
 
     } // fin de main
-    
-public double iMasaCorporal( double n, double m ) {
-    double resultado;
-    resultado = n / Math.pow( m, 2 );
-    
-    if ( resultado < 18.5 ) {
-        return ()
-}
-}
-    
+
+    public static double iMasaCorporal( double n, double m ) {
+        double resultado;
+        resultado = n / Math.pow( m, 2 );
+
+        return ( resultado );
+    }
+
 } // fin de la clase MaisdeStrings_9_JMS
 
