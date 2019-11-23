0768f9b8f2e2003712ae2f754781f251199e4c0e
llllJokerllll <Yosi15@gmail.com>
2019-11-23 12:03:51 +0100

    update

--- Trimestre1/Java/Ejercicios_AceptaElReto/src/AceptaElReto_101.java
+++ Trimestre1/Java/Ejercicios_AceptaElReto/src/AceptaElReto_101.java
@@ -24,9 +24,8 @@ public class AceptaElReto_101 {
         // Salida de datos
         // Operativa
         int contador1, contador2;
-        int sumaVertical, sumaHoriz, sumaDiag1, sumaDiag2;
-        int cm2, numMax;
-        int esquinas, centro, centroLados;
+        int sumaVertical, sumaVertical2, sumaHoriz, sumaHoriz2, sumaDiag1, sumaDiag2;
+        int cm2, numMax, mitadCuadrado, mitadCuadrado2;
         // Constantes
         // Inicializaciónç
         // Creación de objetos
@@ -37,11 +36,10 @@ public class AceptaElReto_101 {
             tLados = entrada.nextInt();
             if ( tLados == 0 ) break;
             int[][] cuadrado = new int [ tLados ][ tLados ];
-            sumaVertical = sumaHoriz = sumaDiag1 = sumaDiag2 = 0;
-            esquinas = centro = centroLados = 0;
+            sumaVertical = sumaVertical2 = sumaHoriz = sumaHoriz2 = sumaDiag1 = sumaDiag2 = 0;
             numMax = 0;
             if ( tLados % 2 != 0 ) {
-                int mitadCuadrado = tLados / 2;
+                mitadCuadrado = tLados / 2;
             
                 for ( contador1 = 0; contador1 < tLados; contador1++ ) {
                     for ( contador2 = 0; contador2 < tLados; contador2++ ) {
@@ -63,20 +61,20 @@ public class AceptaElReto_101 {
                         }
                     }
                 }
-                for ( int[] x : cuadrado ) {
-                    for ( int y : x ) {
-                        System.out.print( y  + " " );
-                    }
-                    System.out.println( "" );
-                }
-                System.out.println( sumaDiag1 );
-                System.out.println( sumaDiag2 );
-                System.out.println( sumaHoriz );
-                System.out.println( sumaVertical );
+//                for ( int[] x : cuadrado ) {
+//                    for ( int y : x ) {
+//                        System.out.print( y  + " " );
+//                    }
+//                    System.out.println( "" );
+//                }
+//                System.out.println( sumaDiag1 );
+//                System.out.println( sumaDiag2 );
+//                System.out.println( sumaHoriz );
+//                System.out.println( sumaVertical );
                 
                 if ( sumaDiag1 == sumaVertical && sumaDiag1 == sumaHoriz && sumaDiag1 == sumaDiag2 ) {
                     cm2 = 4 * sumaHoriz / tLados;
-                    System.out.println( cm2 );
+//                    System.out.println( cm2 );
                     if ( numMax <= tLados * tLados && cuadrado[ mitadCuadrado ][ mitadCuadrado ] * 4 == cm2 && cuadrado[ 0 ][ mitadCuadrado ] + cuadrado[ mitadCuadrado ][ 0 ] + cuadrado[ tLados - 1 ][ mitadCuadrado ] + cuadrado[ mitadCuadrado ][ tLados - 1 ] == cm2 && cuadrado[ 0 ][ 0 ] + cuadrado[ 0 ][ tLados - 1 ] + cuadrado[ tLados - 1 ][ 0 ] + cuadrado[ tLados - 1 ][ tLados - 1 ] == cm2 ) {
                         System.out.println( "ESOTERICO" );
                     } else {
@@ -84,6 +82,30 @@ public class AceptaElReto_101 {
                     }
                 } else {
                     System.out.println( "NO" );
+                }
+            } else {
+                mitadCuadrado = tLados / 2;
+                mitadCuadrado2 = mitadCuadrado + 1;
+                
+                for ( contador1 = 0; contador1 < tLados; contador1++ ) {
+                    for ( contador2 = 0; contador2 < tLados; contador2++ ) {
+                        cuadrado[ contador1 ][ contador2 ] = entrada.nextInt();
+                        if ( cuadrado[ contador1 ][ contador2 ] > numMax ) {
+                            numMax = cuadrado[ contador1 ][ contador2 ];
+                        }
+                        if ( contador1 == contador2 ) {
+                            sumaDiag1 += cuadrado[ contador1 ][ contador2 ];
+                        }
+                        if ( mitadCuadrado == contador2 ) {
+                            sumaVertical += cuadrado[ contador1 ][ contador2 ];
+                        } 
+                        if ( mitadCuadrado == contador1 ) {
+                            sumaHoriz += cuadrado[ contador1 ][ contador2 ];
+                        }
+                        if ( contador1 + contador2 == tLados - 1 ) {
+                            sumaDiag2 += cuadrado[ contador1 ][ contador2 ];
+                        }
+                    }
                 }
             }
             
