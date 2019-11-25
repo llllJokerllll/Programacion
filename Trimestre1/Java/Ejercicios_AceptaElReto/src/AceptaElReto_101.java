014abe3c9ec75ea63ca62db4ae0a80023c256bad
llllJokerllll <Yosi15@gmail.com>
2019-11-25 12:50:24 +0100

    update

--- Trimestre1/Java/Ejercicios_AceptaElReto/src/AceptaElReto_101.java
+++ Trimestre1/Java/Ejercicios_AceptaElReto/src/AceptaElReto_101.java
@@ -24,7 +24,7 @@ public class AceptaElReto_101 {
         // Salida de datos
         // Operativa
         int contador1, contador2;
-        int sumaVertical, sumaVertical2, sumaHoriz, sumaHoriz2, sumaDiag1, sumaDiag2;
+        int sumaVertical, sumaHoriz, sumaDiag1, sumaDiag2;
         int cm2, numMax, mitadCuadrado, mitadCuadrado2;
         // Constantes
         // Inicializaciónç
@@ -36,7 +36,7 @@ public class AceptaElReto_101 {
             tLados = entrada.nextInt();
             if ( tLados == 0 ) break;
             int[][] cuadrado = new int [ tLados ][ tLados ];
-            sumaVertical = sumaVertical2 = sumaHoriz = sumaHoriz2 = sumaDiag1 = sumaDiag2 = 0;
+            sumaVertical = sumaHoriz = sumaDiag1 = sumaDiag2 = 0;
             numMax = 0;
             if ( tLados % 2 != 0 ) {
                 mitadCuadrado = tLados / 2;
@@ -85,7 +85,7 @@ public class AceptaElReto_101 {
                 }
             } else {
                 mitadCuadrado = tLados / 2;
-                mitadCuadrado2 = mitadCuadrado + 1;
+                mitadCuadrado2 = mitadCuadrado - 1;
                 
                 for ( contador1 = 0; contador1 < tLados; contador1++ ) {
                     for ( contador2 = 0; contador2 < tLados; contador2++ ) {
@@ -107,6 +107,28 @@ public class AceptaElReto_101 {
                         }
                     }
                 }
+//                for ( int[] x : cuadrado ) {
+//                    for ( int y : x ) {
+//                        System.out.print( y  + " " );
+//                    }
+//                    System.out.println( "" );
+//                }
+//                System.out.println( sumaDiag1 );
+//                System.out.println( sumaDiag2 );
+//                System.out.println( sumaHoriz );
+//                System.out.println( sumaVertical );
+                if ( sumaDiag1 == sumaVertical && sumaDiag1 == sumaHoriz && sumaDiag1 == sumaDiag2 ) {
+                    cm2 = cuadrado[ mitadCuadrado ][ mitadCuadrado ] + cuadrado[ mitadCuadrado ][ mitadCuadrado2 ] + cuadrado[ mitadCuadrado2 ][ mitadCuadrado ] + cuadrado[ mitadCuadrado2 ][ mitadCuadrado2 ];
+//                    System.out.println( cm2 );
+                    if ( numMax <= tLados * tLados && cuadrado[ 0 ][ mitadCuadrado ] + cuadrado[ mitadCuadrado ][ 0 ] + cuadrado[ 0 ][ mitadCuadrado2 ] + cuadrado[ mitadCuadrado2 ][ 0 ] + cuadrado[ tLados - 1 ][ mitadCuadrado ] + cuadrado[ mitadCuadrado ][ tLados - 1 ] + cuadrado[ tLados - 1 ][ mitadCuadrado2 ] + cuadrado[ mitadCuadrado2 ][ tLados - 1 ] == cm2 * 2 && cuadrado[ 0 ][ 0 ] + cuadrado[ 0 ][ tLados - 1 ] + cuadrado[ tLados - 1 ][ 0 ] + cuadrado[ tLados - 1 ][ tLados - 1 ] == cm2 ) {
+                        System.out.println( "ESOTERICO" );
+                    } else {
+                        System.out.println( "DIABOLICO" );
+                    }
+                } else {
+                    System.out.println( "NO" );
+                }
+                
             }
             
         }
--- Trimestre1/Java/Ejercicios_AceptaElReto/src/AceptaElreto_176.java
+++ Trimestre1/Java/Ejercicios_AceptaElReto/src/AceptaElreto_176.java
@@ -1,148 +1,146 @@
 /*
  * Documento creado no 2019 por DAWDU10
- * CIFP A Carballeira. Proyecto Ejercicios_AceptaElReto en 22 nov. 2019
+ * CIFP A Carballeira. Proyecto Ejercicios_AceptaElReto en 25 nov. 2019
  * Contenido con licencia Creative Commons CC BY-NC-SA.
  */
 
 /**
  * @author Jose Manuel Sabarís García <Josemsabaris@gmail.com>
- * Título: Ejercicio AceptaElReto_207
+ * Título: Ejercicio AceptaElreto_176
  * @version 1.0
  */
-
 // Importación de librerías
 import java.util.Scanner;
 
-public class AceptaElReto_207 {
+public class AceptaElreto_176 {
 
     public static void main( String[] args ) {
-        
+
         // Declaración de variables
         // Entrada de datos
-        int alto, ancho, nArboles; 
+        int alto, ancho;
         // Salida de datos
-        int totalSombra;
+        int resultado;
         // Operativa
         int contador1, contador2, contador3, contador4;
+        int aux;
         // Constantes
-        // Inicialización
-        totalSombra = 0;
-        // Creación de objetos
+        // Inicialización        
+        // Creación de 
         Scanner entrada = new Scanner( System.in );
-        
-        // Entrada de datos, resolución del programa y salida de datos
+
+        // Entrada de datos, resolución del programa y salida
         while ( true ) {
             ancho = entrada.nextInt();
             alto = entrada.nextInt();
-            nArboles = entrada.nextInt();
-            if ( alto == 0 && ancho == 0 && nArboles == 0 ) break;
-            int[][] bosque = new int[ alto ][ ancho ];
-            
-            for ( contador1 = 1; contador1 <= nArboles; contador1++ ) {
-                bosque[ entrada.nextInt() - 1 ][ entrada.nextInt() - 1 ] = 1;
+            if ( alto == 0 || ancho == 0 ) {
+                break;
             }
-            
+            String[][] minas = new String[ alto ][ ancho ];
+            resultado = 0;
+
             for ( contador1 = 0; contador1 < alto; contador1++ ) {
                 for ( contador2 = 0; contador2 < ancho; contador2++ ) {
-                    if ( bosque[ contador1 ][ contador2 ] == 1 ) {
+                    minas[ contador1 ][ contador2 ] = entrada.next();
+                }
+            }
+
+            for ( contador1 = 0; contador1 < alto; contador1++ ) {
+                for ( contador2 = 0; contador2 < ancho; contador2++ ) {
+                    if ( "_".equals( minas[ contador1 ][ contador2 ] ) ) {
+                        aux = 0;
                         if ( contador1 == 0 && contador2 == 0 ) {
-                            if ( bosque[ 0 ][ 1 ] != 1 ) {
-                                bosque[ 0 ][ 1 ] = 7;
+                            if ( "*".equals( minas[ 0 ][ 1 ] ) ) {
+                                aux++;
                             }
-                            if ( bosque[ 1 ][ 0 ] != 1 ) {
-                                bosque[ 1 ][ 0 ] = 7;
+                            if ( "*".equals( minas[ 1 ][ 0 ] ) ) {
+                                aux++;
                             }
-                            if ( bosque[ 1 ][ 1 ] != 1 ) {
-                                bosque[ 1 ][ 1 ] = 7;
+                            if ( "*".equals( minas[ 1 ][ 1 ] ) ) {
+                                aux++;
                             }
                         } else if ( contador1 == 0 && contador2 == ancho - 1 ) {
-                            if ( bosque[ 0 ][ ancho - 2 ] != 1 ) {
-                                bosque[ 0 ][ ancho - 2 ] = 7;
+                            if ( "*".equals( minas[ 0 ][ ancho - 2 ] ) ) {
+                                aux++;
                             }
-                            if ( bosque[ 1 ][ ancho - 1 ] != 1 ) {
-                                bosque[ 1 ][ ancho - 1 ] = 7;
+                            if ( "*".equals( minas[ 1 ][ ancho - 1 ] ) ) {
+                                aux++;
                             }
-                            if ( bosque[ 1 ][ ancho - 2 ] != 1 ) {
-                                bosque[ 1 ][ ancho - 2 ] = 7;
+                            if ( "*".equals( minas[ 1 ][ ancho - 2 ] ) ) {
+                                aux++;
                             }
                         } else if ( contador1 == alto - 1 && contador2 == 0 ) {
-                            if ( bosque[ alto - 1 ][ 1 ] != 1 ) {
-                                bosque[ alto - 1 ][ 1 ] = 7;
+                            if ( "*".equals( minas[ alto - 1 ][ 1 ] ) ) {
+                                aux++;
                             }
-                            if ( bosque[ alto - 2 ][ 0 ] != 1 ) {
-                                bosque[ alto - 2 ][ 0 ] = 7;
+                            if ( "*".equals( minas[ alto - 2 ][ 0 ] ) ) {
+                                aux++;
                             }
-                            if ( bosque[ alto - 2 ][ 1 ] != 1 ) {
-                                bosque[ alto - 2 ][ 1 ] = 7;
+                            if ( "*".equals( minas[ alto - 2 ][ 1 ] ) ) {
+                                aux++;
                             }
                         } else if ( contador1 == alto - 1 && contador2 == ancho - 1 ) {
-                            if ( bosque[ alto - 1 ][ ancho - 2 ] != 1 ) {
-                                bosque[ alto - 1 ][ ancho - 2 ] = 7;
+                            if ( "*".equals( minas[ alto - 1 ][ ancho - 2 ] ) ) {
+                                aux++;
                             }
-                            if ( bosque[ alto - 2 ][ ancho - 1 ] != 1 ) {
-                                bosque[ alto - 2 ][ ancho - 1 ] = 7;
+                            if ( "*".equals( minas[ alto - 2 ][ ancho - 1 ] ) ) {
+                                aux++;
                             }
-                            if ( bosque[ alto - 2 ][ ancho - 2 ] != 1 ) {
-                                bosque[ alto - 2 ][ ancho - 2 ] = 7;
+                            if ( "*".equals( minas[ alto - 2 ][ ancho - 2 ] ) ) {
+                                aux++;
                             }
                         } else if ( contador1 == 0 && contador2 > 0 && contador2 < ancho - 1 ) {
                             for ( contador3 = 0; contador3 <= 1; contador3++ ) {
                                 for ( contador4 = contador2 - 1; contador4 <= contador2 + 1; contador4++ ) {
-                                    if ( bosque[ contador3 ][ contador4 ] != 1 ) {
-                                        bosque[ contador3 ][ contador4 ] = 7;
+                                    if ( "*".equals( minas[ contador3 ][ contador4 ] ) ) {
+                                        aux++;
                                     }
                                 }
                             }
                         } else if ( contador2 == 0 && contador1 > 0 && contador1 < alto - 1 ) {
                             for ( contador3 = 0; contador3 <= 1; contador3++ ) {
                                 for ( contador4 = contador1 - 1; contador4 <= contador1 + 1; contador4++ ) {
-                                    if ( bosque[ contador4 ][ contador3 ] != 1 ) {
-                                        bosque[ contador4 ][ contador3 ] = 7;
+                                    if ( "*".equals( minas[ contador3 ][ contador4 ] ) ) {
+                                        aux++;
                                     }
                                 }
                             }
                         } else if ( contador1 == alto - 1 && contador2 > 0 && contador2 < ancho - 1 ) {
                             for ( contador3 = alto - 1; contador3 >= alto - 2; contador3-- ) {
                                 for ( contador4 = contador2 - 1; contador4 <= contador2 + 1; contador4++ ) {
-                                    if ( bosque[ contador3 ][ contador4 ] != 1 ) {
-                                        bosque[ contador3 ][ contador4 ] = 7;
+                                    if ( "*".equals( minas[ contador3 ][ contador4 ] ) ) {
+                                        aux++;
                                     }
                                 }
                             }
                         } else if ( contador2 == ancho - 1 && contador1 > 0 && contador1 < alto - 1 ) {
                             for ( contador3 = ancho - 1; contador3 >= ancho - 2; contador3-- ) {
                                 for ( contador4 = contador1 - 1; contador4 <= contador1 + 1; contador4++ ) {
-                                    if ( bosque[ contador4 ][ contador3 ] != 1 ) {
-                                        bosque[ contador4 ][ contador3 ] = 7;
+                                    if ( "*".equals( minas[ contador3 ][ contador4 ] ) ) {
+                                        aux++;
                                     }
                                 }
                             }
                         } else {
                             for ( contador3 = contador1 - 1; contador3 <= contador1 + 1; contador3++ ) {
                                 for ( contador4 = contador2 - 1; contador4 <= contador2 + 1; contador4++ ) {
-                                    if ( bosque[ contador3 ][ contador4 ] != 1 ) {
-                                        bosque[ contador3 ][ contador4 ] = 7;
+                                    if ( "*".equals( minas[ contador3 ][ contador4 ] ) ) {
+                                        aux++;
                                     }
                                 }
                             }
                         }
+
+                        if ( aux >= 6 ) {
+                            resultado++;
+                        }
                     }
                 }
             }
-            
-            for ( int[] x : bosque ) {
-                for ( int y : x ) {
-//                System.out.print( y + " " );
-                    if ( y == 7 ) {
-                       totalSombra++; 
-                    }
-                }
-//               System.out.println( "" );
-            }
-            System.out.println( totalSombra );
+            System.out.println( resultado );
         }
 
     } // fin de main
-    
-} // fin de la clase AceptaElReto_207
+
+} // fin de la clase AceptaElreto_176
 
