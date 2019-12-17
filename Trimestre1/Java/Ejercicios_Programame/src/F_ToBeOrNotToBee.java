14530fa11f6e7add962b0509ab8a840bddbf990f
llllJokerllll <Yosi15@gmail.com>
2019-12-17 19:31:22 +0100

    update

--- Trimestre1/Java/Ejercicios_Programame/src/F_ToBeOrNotToBee.java
+++ Trimestre1/Java/Ejercicios_Programame/src/F_ToBeOrNotToBee.java
@@ -9,66 +9,104 @@
  * Título: Ejercicio F_ToBeOrNotToBee
  * @version 1.0
  */
-
 // Importación de librerías
 import java.util.Scanner;
 
 public class F_ToBeOrNotToBee {
 
     public static void main( String[] args ) {
-        
+
         // Declaración de variables
         // Entrada de datos
         int lado;
         // Salida de datos
+        int velutinas, abejasDestro, cuadriculasDestro;
         // Operativa
         // Constantes
-        // Inicialización        
+        // Inicialización
+        velutinas = abejasDestro = cuadriculasDestro = 0;
         // Creación de objetos
         Scanner entrada = new Scanner( System.in );
-        
+
         // Entrada de datos y resolución del programa
         lado = entrada.nextInt();
         int[][] panalAbejas = new int[ lado ][ lado ];
         int[][] panalVelutina = new int[ lado ][ lado ];
-            for ( int i = 0; i < lado; i++ ) {
-                for ( int j = 0; j < lado; j++ ) {
-                    panalAbejas[ i ][ j ] = entrada.nextInt();
-                    if ( fila > 0 ) {
-                        bosque[ fila - 1 ][ columna ] = 1;
-                        if ( columna > 0 ) {
-                            bosque[ fila - 1 ][ columna - 1 ] = 1;
-                        }
-
-                        if ( columna < ancho - 1 ) {
-                            bosque[ fila - 1 ][ columna + 1 ] = 1;
-                        }
-                    }
-                    if ( columna < ancho - 1 ) {
-                        bosque[ fila ][ columna + 1 ] = 1;
-                    }
-
-                    if ( columna > 0 ) {
-                        bosque[ fila ][ columna - 1 ] = 1;
-                        if ( fila < alto - 1 ) {
-                            bosque[ fila + 1 ][ columna - 1 ] = 1;
-                        }
-                    }
-                    if ( fila < alto - 1 ) {
-                        bosque[ fila + 1 ][ columna ] = 1;
-                    }
-                    if ( columna < ancho - 1 && fila < alto - 1 ) {
-                        bosque[ fila + 1 ][ columna + 1 ] = 1;
-                    }
-
-                }
-
+        for ( int i = 0; i < lado; i++ ) {
+            for ( int j = 0; j < lado; j++ ) {
+                panalAbejas[ i ][ j ] = entrada.nextInt();
             }
-        
+        }
+        for ( int i = 0; i < lado; i++ ) {
+            for ( int j = 0; j < lado; j++ ) {
+                panalVelutina[ i ][ j ] = entrada.nextInt();
+                velutinas += panalVelutina[ i ][ j ];
+                for ( int k = 0; k < panalVelutina[ i ][ j ]; k++ ) {
+                    if ( i > 0 ) {
+                       if ( panalAbejas[ i - 1 ][ j ] != 0 ) {
+                           abejasDestro += panalAbejas[ i - 1 ][ j ];
+                           cuadriculasDestro++;
+                           panalAbejas[ i - 1 ][ j ] = 0;
+                       }
+                        if ( j > 0 ) {
+                            if ( panalAbejas[ i - 1 ][ j - 1 ] != 0 ) {
+                                abejasDestro += panalAbejas[ i - 1 ][ j - 1 ];
+                                cuadriculasDestro++;
+                                panalAbejas[ i - 1 ][ j - 1 ] = 0;
+                            }
+                        }
+
+                        if ( j < lado - 1 ) {
+                            if ( panalAbejas[ i - 1 ][ j + 1 ] != 0 ) {
+                                abejasDestro += panalAbejas[ i - 1 ][ j + 1 ];
+                                cuadriculasDestro++;
+                                panalAbejas[ i - 1 ][ j - 1 ] = 0;
+                            }
+                        }
+                    }
+                    if ( j < lado - 1 ) {
+                        if ( panalAbejas[ i ][ j + 1 ] != 0 ) {
+                            abejasDestro += panalAbejas[ i ][ j + 1 ];
+                            cuadriculasDestro++;
+                            panalAbejas[ i ][ j + 1 ] = 0;
+                        }
+                    }
+
+                    if ( j > 0 ) {
+                        if ( panalAbejas[ i ][ j - 1 ] != 0 ) {
+                            abejasDestro += panalAbejas[ i ][ j - 1 ];
+                            cuadriculasDestro++;
+                            panalAbejas[ i ][ j - 1 ] = 0;
+                        }
+                        if ( i < lado - 1 ) {
+                            if ( panalAbejas[ i + 1 ][ j - 1 ] != 0 ) {
+                                abejasDestro += panalAbejas[ i + 1 ][ j - 1 ];
+                                cuadriculasDestro++;
+                                panalAbejas[ i + 1 ][ j - 1 ] = 0;
+                            }
+                        }
+                    }
+                    if ( i < lado - 1 ) {
+                        if ( panalAbejas[ i + 1 ][ j ] != 0 ) {
+                            abejasDestro += panalAbejas[ i + 1 ][ j ];
+                            cuadriculasDestro++;
+                            panalAbejas[ i + 1 ][ j ] = 0;
+                        }
+                    }
+                    if ( j < lado - 1 && i < lado - 1 ) {
+                        if ( panalAbejas[ i + 1 ][ j + 1 ] != 0 ) {
+                            abejasDestro += panalAbejas[ i + 1 ][ j + 1 ];
+                            cuadriculasDestro++;
+                            panalAbejas[ i + 1 ][ j + 1 ] = 0;
+                        }
+                    }
+                }
+            }
+        }
         // Salida de datos
-
-
+        System.out.println( velutinas + " " + abejasDestro + " " + cuadriculasDestro );
+        
     } // fin de main
-    
+
 } // fin de la clase F_ToBeOrNotToBee
 
