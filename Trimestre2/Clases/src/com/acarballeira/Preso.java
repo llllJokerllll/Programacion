32c04f5548dfbbf6b74ee87df83ea37cbb93f995
llllJokerllll <Yosi15@gmail.com>
2020-01-13 17:28:58 +0100

    update

--- Trimestre2/Clases/src/com/acarballeira/Preso.java
+++ Trimestre2/Clases/src/com/acarballeira/Preso.java
@@ -2,15 +2,80 @@ package com.acarballeira;
 
 public class Preso {
 
-    private String nome;
+    // Propiedades de la clase
+    private String nome = ""; // Aquí usamos el igual para dejar un valor default que nosotros escojamos.
     private String apodo;
     private String codigo;
     private int anosSentencia;
+    private static int numPresos = 0;
 
-    public Preso( String nome, String apodo, String codigo, int anosSentencia ) {
-        this.nome = nome;
+    // Constructores de la clase
+    public Preso() {
+        numPresos++;
+        Preso.numPresos++;
+    }
+    
+    public Preso( String apodo, String codigo ) {
+        this();
         this.apodo = apodo;
         this.codigo = codigo;
+    }
+
+    public Preso( String nome, String apodo, String codigo, int anosSentencia ) {
+        this(apodo, codigo);
+        this.nome = nome;
         this.anosSentencia = anosSentencia;
     }
+    
+    // Métodos get & set de cada propiedad de la clase
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getAnosSentencia() {
+        return anosSentencia;
+    }
+
+    public void setAnosSentencia(int anosSentencia) {
+        this.anosSentencia = anosSentencia;
+    }
+    
+    public int getNumPresos() {
+        return numPresos;
+    }
+    
+    // Método toString para imprimir las propiedades de la clase
+    @Override
+    public String toString() {
+        String res = "";
+        
+        res += "Nome: " + this.nome + "\n";
+        res += "Apodo: " + this.apodo + "\n";
+        res += "Código: " + this.codigo + "\n";
+        res += "Sentencia: " + this.anosSentencia + "\n";
+        res += "Nº de preso: " + numPresos + "\n";
+        
+        return res;
+    }
+
 }
--- Trimestre2/Clases/src/com/acarballeira2/PresoDriver.java
+++ Trimestre2/Clases/src/com/acarballeira2/PresoDriver.java
@@ -5,19 +5,22 @@ import com.acarballeira.Preso;
 public class PresoDriver {
 
     public static void main(String[] args) {
-
-        Preso p1, p2, p3;
         
-        p1 = new Preso("Pablo","Pableras","P007",69);
+        Preso p1 = new Preso("Pablo","Pableras","P007",69);
         
-        p2 = new Preso("Jose","Joker","P009",15);
+        Preso p2 = new Preso("Jose","Joker","P009",15);
         
-        p3 = new Preso("Oscar","Navaja","P008",32);
+        Preso p3 = new Preso("Oscar","Navaja","P008",32);
+        
+        Preso p4 = new Preso();
+        
+        Preso p5 = new Preso("Farlopa", "P010");
         
         System.out.println( p1 );
         System.out.println( p2 );
         System.out.println( p3 );
-        
+        System.out.println( p4 );
+        System.out.println( p5 );
 
     }
 
