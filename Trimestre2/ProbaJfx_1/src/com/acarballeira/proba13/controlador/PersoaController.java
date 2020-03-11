848db9e975a76e70749ca04b9017f37f012b78a6
llllJokerllll <Yosi15@gmail.com>
2020-03-11 22:01:38 +0100

    update

--- Trimestre2/ProbaJfx_1/src/com/acarballeira/proba13/controlador/PersoaController.java
+++ Trimestre2/ProbaJfx_1/src/com/acarballeira/proba13/controlador/PersoaController.java
@@ -1,23 +1,31 @@
 package com.acarballeira.proba13.controlador;
 
+
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.acarballeira.proba12.modelo.Persoa;
+import com.acarballeira.proba13.modelo.Persoa;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 
 public class PersoaController implements Initializable {
@@ -68,17 +76,33 @@ public class PersoaController implements Initializable {
     @FXML
     private void agregarPersona(ActionEvent event) {
         
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/acarballeira/proba13/vista/PersoaDialogScene"));
+        
+        Pane ventanaModal = null;
+        
         try {
-            Persoa p = new Persoa(this.txtNombre.getText(), this.txtApellidos.getText(), Integer.parseInt(this.txtEdad.getText()));
-            this.personas.add(p);
-            this.tblPersonas.setItems(this.personas);
-        } catch (NumberFormatException e) {
+            ventanaModal = loader.load();
+        } catch (IOException e) {
             Alert alert = new Alert(AlertType.WARNING);
             alert.setHeaderText(null);
             alert.setTitle("Error");
             alert.setContentText("Algún dato con formato incorrecto");
             alert.showAndWait();
         }
+        PersoaDialogController controlador = loader.getController();
+        controlador.inicializarAtributos(this.personas);
+        Scene scene = new Scene(ventanaModal);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+        
+        Persoa p = controlador.getPersona();
+        if (p != null) {
+            this.personas.add(p);
+            this.tblPersonas.refresh();
+        }
+       
     }
     
     @FXML
@@ -142,6 +166,7 @@ public class PersoaController implements Initializable {
     public void initialize(URL arg0, ResourceBundle arg1) {
 
         this.personas = FXCollections.observableArrayList();
+        
         
         colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
         colApellidos.setCellValueFactory(new PropertyValueFactory("apellidos"));
--- Trimestre2/ProbaJfx_1/src/com/acarballeira/proba13/controlador/PersoaDialogController.java
+++ Trimestre2/ProbaJfx_1/src/com/acarballeira/proba13/controlador/PersoaDialogController.java
@@ -1,6 +1,84 @@
 package com.acarballeira.proba13.controlador;
 
-public class PersoaDialogController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import com.acarballeira.proba13.modelo.Persoa;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+public class PersoaDialogController implements Initializable {
+
+    @FXML
+    private TextField txtEdad;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtApellidos;
+    
+    @FXML
+    private TextField btnGuardar;
+    @FXML
+    private TextField btnSalir;
+    
+    private Persoa persona;
+    
+    private ObservableList<Persoa> personas;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        
+    }
+
+    public void inicializarAtributos(ObservableList<Persoa> personas) {
+        this.personas = personas;
+        this.persona = pSeleccionada;
+        
+        
+    }
+    
+    @FXML
+    private void salir(ActionEvent event) {
+        this.persona = null;
+        Stage stage = (Stage) this.btnGuardar.getScene().getWindow();
+        stage.close();
+    }
+    
+    @FXML
+    private void guardar(ActionEvent event) {
+        
+        try {
+            Persoa p = new Persoa(this.txtNombre.getText(), this.txtApellidos.getText(), Integer.parseInt(this.txtEdad.getText()));
+            if (this.personas.contains(p)) {
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.setHeaderText(null);
+                alert.setTitle("Error");
+                alert.setContentText("Persoa xa existente");
+                alert.showAndWait();
+            } else {
+                this.persona = p;
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setTitle("Perfecto");
+                alert.setContentText("Persoa creada correctamente");
+                alert.showAndWait();
+            }
+            
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("Algún dato con formato incorrecto");
+            alert.showAndWait();
+        }
+    }
     
 }
